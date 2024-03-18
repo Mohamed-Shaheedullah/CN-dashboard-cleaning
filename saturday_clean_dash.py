d46a6a3be36eedf904754d300fd1756a63a476b6
Mohamed Shaheedullah <conifer567@outlook.com>
2024-03-18 15:10:12 +0000

    first commit

---
+++ saturday_clean_dash.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import openpyxl
+import numpy as np
+import matplotlib.pyplot as plt
+
+df1 = pd.read_excel(r"./mb_dash_week/saturday_data.xlsx")
+
+## check for duplicates
+duplicate_values = df1.duplicated()
+print(f"duplicates {duplicate_values.value_counts()}")
+
+# print(df1.head())
+
+# print(df1.info())
+
+# print(df1.describe())
+
+df1 = df1.dropna(how="any")
+
+df1 = df1.drop(columns=["Till ID"])
+
+print(df1.head())
+
+print(df1.info())
+
+print(df1.describe())
+
+b_plot = df1.boxplot(column = 'Cost') 
+plt.title("Boxplot for Cost")
+plt.show()
+
+b_plot_2 = df1.boxplot(column = 'Total Items') 
+plt.title("Boxplot for Total Items")
+plt.show()
+
+b_plot_3 = df1.boxplot(column = 'Transaction ID') 
+plt.title("Boxplot for Transaction ID")
+plt.show()
---
+++ thursday_clean_dash.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import openpyxl
+import numpy as np
+import matplotlib.pyplot as plt
+
+df1 = pd.read_excel(r"./mb_dash_week/thursday_data.xlsx")
+
+## check for duplicates
+duplicate_values = df1.duplicated()
+print(f"duplicates {duplicate_values.value_counts()}")
+
+
+print(df1.head())
+
+print(df1.info())
+
+print(df1.describe())
+
+df1 = df1.dropna(how="any")
+
+df1 = df1.drop(columns=["Till ID"])
+
+print(df1.head())
+
+print(df1.info())
+
+print(df1.describe())
+
+b_plot = df1.boxplot(column = 'Cost') 
+plt.title("Boxplot for Cost")
+plt.show()
+
+b_plot_2 = df1.boxplot(column = 'Total Items') 
+plt.title("Boxplot for Total Items")
+plt.show()
+
+b_plot_3 = df1.boxplot(column = 'Transaction ID') 
+plt.title("Boxplot for Transaction ID")
+plt.show()
+
+### outlier found cost 700
+## change outlier
+df1.at[23, "Cost"] = 7
+
+df1.to_excel("./mb_clean_data/thursday_cleaned.xlsx")
+
+print(df1.describe())
