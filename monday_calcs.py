fef40919c808526a67b8c71650e68492fe21a14a
Mohamed Shaheedullah <conifer567@outlook.com>
2024-03-18 15:59:36 +0000

    monday pm class finished

--- monday_calcs.py
+++ monday_calcs.py
@@ -17,4 +17,29 @@ mvp_staff_mon = df.groupby('Staff')['Cost'].sum().reset_index()
 
 print(mvp_staff_mon.sort_values(["Cost"], ascending=False).head(1))
 
+##### explode basket for item info, best and worst selling #####
+
+
+def remove_punctuation(basket):
+    basket = str(basket)
+    basket=basket.replace("[", "")
+    basket=basket.replace("]", "")
+    basket=basket.replace("'", "")
+    return basket
+
+df["Basket"] = df["Basket"].apply(remove_punctuation)
+
+def split_basket(basket_str):
+    elements = basket_str.split(",")
+    return [item.strip() for item in elements]
+
+df["Basket"] = df["Basket"].apply(split_basket)
+
+df = df.explode("Basket", ignore_index=False)
+
+
+best_sell_mon = df["Basket"].value_counts().head(1)
+print(f"best_sell_mon is {best_sell_mon}")
+
+###### WORST SELL MONDAY BELOW ####
 
