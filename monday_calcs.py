ed229e65ba8b0ad1d1b7312fecd9968b8be9604c
Mohamed Shaheedullah <conifer567@outlook.com>
2024-03-19 10:42:43 +0000

    Monday calcs and file done, working

--- monday_calcs.py
+++ monday_calcs.py
@@ -2,6 +2,8 @@ import pandas as pd
 import openpyxl
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
+from time import gmtime, strftime
 
 df = pd.read_excel("./mb_clean_data/monday_cleaned.xlsx")
 
@@ -40,8 +42,36 @@ df["Basket"] = df["Basket"].apply(split_basket)
 
 df = df.explode("Basket", ignore_index=False)
 
+#### end explode *************************
+
+
 best_sell_mon = df["Basket"].value_counts().head(1)
-print(f"best sell mon is {best_sell_mon}")
+print(f"best sell item mon is {best_sell_mon}")
 
 worst_sell_mon = df["Basket"].value_counts().tail(1)
-print(f"worst sell mon is {worst_sell_mon}")
+print(f"worst sell item mon is {worst_sell_mon}")
+
+
+### **********write to file *********
+# actual_time = strftime("%Y-%m-%d %H-%M-%S", gmtime())
+
+filename = "./mb_results/monday_results.txt"
+
+outfile = open(filename, "w")
+with open(filename, 'a') as outfile:
+    outfile.write('\n')
+    outfile.write("Total Income : ")
+    outfile.write(str(total_income_mon))    
+    outfile.write('\n')
+    outfile.write("Highest spend : ")
+    outfile.write(str(higest_spend_mon))
+    outfile.write('\n')
+    outfile.write("MVP staff for Monday")
+    outfile.write(str(mvp_top))
+    outfile.write('\n')
+    outfile.write("Best selling item : ")
+    outfile.write(str(best_sell_mon))
+    outfile.write('\n')
+    outfile.write("Worst selling item : ")
+    outfile.write(str(worst_sell_mon))
+    outfile.write('\n')
